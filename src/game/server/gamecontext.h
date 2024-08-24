6a38524feb4f308e8f31059230ed546d27d38af6
bencie <benciee0@gmail.com>
2024-08-24 12:42:39 +0200

    +setvel

--- src/game/server/gamecontext.h
+++ src/game/server/gamecontext.h
@@ -411,6 +411,7 @@ private:
 	static void ConGoDown(IConsole::IResult *pResult, void *pUserData);
 	static void ConMove(IConsole::IResult *pResult, void *pUserData);
 	static void ConMoveRaw(IConsole::IResult *pResult, void *pUserData);
+	static void ConSetVel(IConsole::IResult *pResult, void *pUserData);
 
 	static void ConToTeleporter(IConsole::IResult *pResult, void *pUserData);
 	static void ConToCheckTeleporter(IConsole::IResult *pResult, void *pUserData);
