1d6f417a6382ef83d2eacf6c4f1e67745b208dfa
bencie <benciee0@gmail.com>
2024-09-09 15:27:27 +0200

    dont unfreeze on tele_all

--- src/game/server/ddracecommands.cpp
+++ src/game/server/ddracecommands.cpp
@@ -503,7 +503,6 @@ void CGameContext::ConTeleportAll(IConsole::IResult *pResult, void *pUserData)
 		{
 			pSelf->Teleport(pChr, Pos);
 			pChr->ResetJumps();
-			pChr->UnFreeze();
 			pChr->SetVelocity(vec2(0, 0));
 		}
 	}
